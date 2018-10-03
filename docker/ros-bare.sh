98bf5985f0f049a96ffa91206081f110a2d3b48d
Ulysse VAUTIER <ulysse.vautier@plymouth.ac.uk>
2018-10-03 16:56:14 +0100

    oops uncommented commented nonsense

--- docker/ros-bare.sh
+++ docker/ros-bare.sh
@@ -3,14 +3,14 @@
 set -o errexit
 set -o verbose
 
-#echo "deb http://packages.ros.org/ros/ubuntu stretch main" > /etc/apt/sources.list.d/ros-latest.list && 
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-#apt update
-#apt upgrade 
-#apt-get install build-essential python-yaml cmake subversion wget python-setuptools mercurial git-core libapr1-dev libaprutil1-dev libbz2-dev python-dev libgtest-dev python-paramiko libboost-all-dev liblog4cxx10-dev pkg-config python-empy swig python-nose lsb-release python-pip python-gtk2
-#pip install rosdep rosinstall_generator wstool rosinstall
-#rosdep init 
-#rosdep update
+echo "deb http://packages.ros.org/ros/ubuntu stretch main" > /etc/apt/sources.list.d/ros-latest.list && 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt update
+apt upgrade 
+apt-get install build-essential python-yaml cmake subversion wget python-setuptools mercurial git-core libapr1-dev libaprutil1-dev libbz2-dev python-dev libgtest-dev python-paramiko libboost-all-dev liblog4cxx10-dev pkg-config python-empy swig python-nose lsb-release python-pip python-gtk2
+pip install rosdep rosinstall_generator wstool rosinstall
+rosdep init 
+rosdep update
 cd /var/tmp/
 git clone https://github.com/Rob-Orb/Ros.git
 cd Ros
