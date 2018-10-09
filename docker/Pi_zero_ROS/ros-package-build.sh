80d75a4d964977727cd9c78f0f43e3196fb716e6
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2018-10-09 16:59:50 +0200

    fix(Docker): Docker ROS

--- docker/Pi_zero_ROS/ros-package-build.sh
+++ docker/Pi_zero_ROS/ros-package-build.sh
@@ -3,17 +3,7 @@
 set -o errexit
 set -o verbose
 
-wget -qO - https://rob-orb.github.io/debian/pub.key | sudo apt-key add -
-echo "deb http://rob-orb.github.io/debian stretch main" > /etc/apt/sources.list.d/rob-orb.list
-echo "deb http://packages.ros.org/ros/ubuntu stretch main" > /etc/apt/sources.list.d/ros-latest.list && 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-apt update
-apt install ros-kinetic-bare
-/opt/ros/kinetic/initialize.sh
 source /opt/ros/kinetic/setup.bash
-echo "yaml https://rob-orb.github.io/rosdistro/rosdep/ros-rosdistro-kinetic.yaml kinetic" > /etc/ros/rosdep/sources.list.d/90-ros-rosdistro-kinectic.list
-export ROSDISTRO_INDEX_URL=https://rob-orb.github.io/rosdistro/index.yaml
-rosdep update
 cd /var/tmp/
 mkdir catkin_ws
 cd catkin_ws
